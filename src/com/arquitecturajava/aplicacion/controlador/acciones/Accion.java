35244e735dfdaaf75fd96a107ca152a3cf1ec334
eladio <sr.eladio@gmail.com>
2013-11-24 19:59:50 +0100

    sobrecarga del método getBean para utilizar la factoría de Spring específica para aplicación web, y que sólo cargue el fichero de configuración una única vez al arrancar la aplicación

--- src/com/arquitecturajava/aplicacion/controlador/acciones/Accion.java
+++ src/com/arquitecturajava/aplicacion/controlador/acciones/Accion.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  * Nivel más alto de la jerarquía de acciones
@@ -45,8 +47,23 @@ public abstract class Accion {
 	 * @return Instancia del bean. Instancia de la clase servicios
 	 */
 	public Object getBean(String nombre) {
-		// factoría de Spring que busca un fichero dentro del classpath
+		// factoría clásica de Spring que busca un fichero dentro del classpath
 		ClassPathXmlApplicationContext factoria = new ClassPathXmlApplicationContext("contextoAplicacion.xml");
+		return factoria.getBean(nombre);
+	}
+	
+	/**
+	 * Método getBean sobrecargado para que Spring sólo cargue el fichero de
+	 * configuración una única vez al arrancar la aplicación web
+	 * @param nombre Nombre del bean en el fichero contextoAplicacion.xml (serviciosLibro)
+	 * @param request 
+	 * @return Instancia del bean. Instancia de la clase servicios
+	 */
+	public Object getBean(String nombre, HttpServletRequest request) {
+		// factoría específica de Spring a nivel de aplicación web
+		WebApplicationContext factoria = WebApplicationContextUtils.
+				getRequiredWebApplicationContext(request.getSession().
+				getServletContext());
 		return factoria.getBean(nombre);
 	}
 }
